265bf290c01dd709d338ca67a0cc384913e4b224
Brandredo <figueredo407@gmail.com>
2022-01-14 11:36:06 -0500

    fixed fonts

--- myapp/pages/index.js
+++ myapp/pages/index.js
@@ -8,12 +8,6 @@ function Home() {
 
     <>
 
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200;300;400;500&family=Roboto:wght@500;700&display=swap" rel="stylesheet" />
-      </Head>
-
       <section className={styles.welcome}>
 
         <div className={styles.welcomecontainer}>
